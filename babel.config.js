5873030b037e85c50dc6b45e263a5da33aae0a00
Yusuf  Eren Hoşgör <152120211123@ogrenci.ogu.edu.tr>
2025-08-24 17:50:45 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "expo-router/babel",
       // NOTE: This plugin MUST be listed last
       "react-native-reanimated/plugin",
     ],
